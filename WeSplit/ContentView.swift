59c1591db86e7ac610a4bae0ed9ffd80f2bdfc03
Kiren Srinivasan <srinitude@gmail.com>
2020-09-03 13:24:32 -0400

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Kiren Srinivasan on 9/3/20.
+//  Copyright © 2020 Kiren Srinivasan's Practice. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
