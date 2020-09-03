b5e07a4a23b2efc915ff54b2b21c9603e0c04b51
Kiren Srinivasan <srinitude@gmail.com>
2020-09-03 13:45:38 -0400

    Wraps form within a NavigationView

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -10,22 +10,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Form {
-            Section {
-                 Text("Hello, World!")
-                 Text("Hello, World!")
-                 Text("Hello, World!")
-                 Text("Hello, World!")
-                 Text("Hello, World!")
+        NavigationView {
+            Form {
+                Section {
+                     Text("Hello, World!")
+                }
             }
-            
-            Section {
-                 Text("Hello, World!")
-                 Text("Hello, World!")
-                 Text("Hello, World!")
-                 Text("Hello, World!")
-                 Text("Hello, World!")
-            }
+            .navigationBarTitle("SwiftUI", displayMode: .inline)
         }
     }
 }
